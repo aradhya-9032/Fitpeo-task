239714033abc78bdea0be3f16b6279c1ad1ac624
Aradhya <aradhyatimothy012@gmail.com>
2023-10-17 23:09:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-// import React from 'react';
-// import Dashbord from './components/Dashbord';
-// import Navbar from './components/Navbar';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div>
-//      <Dashbord />
-//      <Navbar/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
 
 // App.js
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Dashbord from './components/Dashbord';
-// import Graphbar from './components/Graphbar';
 
 function App() {
   return (
     <div className="app-container">
       <Navbar />
       <Dashbord/>
-      {/* <Graphbar /> */}
     </div>
   );
 }
